62063efce2a84d658279510b0630d967ad270534
Montero <montero@Monteros-MacBook-Pro.local>
2019-11-04 19:23:04 -0600

    Se agrego el parser de la sucursal Kinkin y sus unit tests

--- app/ventas/tests/test_parsers.py
+++ app/ventas/tests/test_parsers.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from ventas.parsers import parser_magno_brasserie
 from ventas.parsers import parser_gambinos_saopaulo
 from ventas.parsers import parser_pecos
+from ventas.parsers import parser_kinkin
 
 from core import models
 import os
@@ -295,6 +296,108 @@ class ParserPecosTests(TestCase):
 
         # Procesamos el reporte de ventas con el parser y guardamos el output en una variable
         output_parser = parser_pecos.parser(path_reporte_ventas, self.pecos)
+        #print(output_parser)
+
+        # Comparamos el output esperado contra el real
+        self.assertEqual(output_esperado, output_parser)
+
+
+class ParserKinkinTests(TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+
+        # Cliente
+        self.operadora_kinkin = models.Cliente.objects.create(nombre='OPERADORA KINKIN')
+        # Sucursal
+        self.kinkin = models.Sucursal.objects.create(nombre='KINKIN', cliente=self.operadora_kinkin)
+        # Almacen
+        self.barra_1 = models.Almacen.objects.create(nombre='BARRA 1', numero=1, sucursal=self.kinkin)
+        # Caja
+        self.caja_1 = models.Caja.objects.create(numero=1, nombre='CAJA 1', almacen=self.barra_1)
+
+    
+    def test_output_ok(self):
+        """ 
+        ----------------------------------------------------------------------
+        Testear que el output del parser es correcto
+        ----------------------------------------------------------------------
+        """
+
+        # Definimos un diccionario con el output esperado del parser
+        output_esperado = [
+            {
+                'sucursal_id': self.kinkin.id,
+                'caja_id': self.caja_1.id,
+                'codigo_pos': 'Hendricks 750 ml',
+                'nombre': 'Hendricks 750 ml',
+                'unidades': 3,
+                'importe': 5400
+            },
+            {
+                'sucursal_id': self.kinkin.id,
+                'caja_id': self.caja_1.id,
+                'codigo_pos': 'Altos Blanco 750ml',
+                'nombre': 'Altos Blanco 750ml',
+                'unidades': 1,
+                'importe': 0
+            },
+            {
+                'sucursal_id': self.kinkin.id,
+                'caja_id': self.caja_1.id,
+                'codigo_pos': 'Don Julio 70 750ml',
+                'nombre': 'Don Julio 70 750ml',
+                'unidades': 1,
+                'importe': 1950
+            },
+            {
+                'sucursal_id': self.kinkin.id,
+                'caja_id': self.caja_1.id,
+                'codigo_pos': 'Absolut Azul 750ml',
+                'nombre': 'Absolut Azul 750ml',
+                'unidades': 1,
+                'importe': 1200
+            }
+        ]
+
+        # Definimos el path absoluto del reporte de ventas
+        path_reporte_ventas = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ventas_kinkin.csv')
+
+        # Procesamos el reporte de ventas con el parser y guardamos el output en una variable
+        output_parser = parser_kinkin.parser(path_reporte_ventas, self.kinkin)
+        #print(output_parser)
+
+        # Convertimos el dataframe del output en un json
+        dataframe_output = output_parser['df_ventas']
+
+        # Tomamos las primeras 4 filas del dataframe
+        dataframe_output = dataframe_output.head(4)
+        #print('::: DATAFRAME OUTPUT :::')
+        #print(dataframe_output)
+
+        output_parser = dataframe_output.to_json(orient='records')
+        # Convertimos el json en un objeto de python
+        output_parser = json.loads(output_parser)
+        # Comparamos el output esperado contra el real
+        self.assertEqual(output_esperado, output_parser)
+
+
+    def test_output_error(self):
+        """
+        ----------------------------------------------------------------------
+        Testear que el parser maneja los errores de forma correcta
+        ----------------------------------------------------------------------
+        """
+
+        # Definimos el output esperado cuando hay un error en el parseo
+        output_esperado = {'df_ventas': {}, 'procesado': False}
+
+        # Definimos el path absoluto del reporte de ventas defectuoso
+        path_reporte_ventas = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'reporte_defectuoso.csv')
+
+        # Procesamos el reporte de ventas con el parser y guardamos el output en una variable
+        output_parser = parser_kinkin.parser(path_reporte_ventas, self.kinkin)
         #print(output_parser)
 
         # Comparamos el output esperado contra el real
